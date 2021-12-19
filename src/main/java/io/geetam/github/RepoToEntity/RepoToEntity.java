a739164c00ea91de92524ecb749e6f98e9c1d5f7
Geetam Chawla <geetam.chawla@gmail.com>
2021-12-19 19:53:27 +0530

    RepoToEntity: Load classes explicitly
    This is needed because of introduction of the
    -benchdir option, classes are no longer guaranteed to
    be in CLASSPATH.

--- src/main/java/io/geetam/github/RepoToEntity/RepoToEntity.java
+++ src/main/java/io/geetam/github/RepoToEntity/RepoToEntity.java
@@ -1,17 +1,39 @@
 package io.geetam.github.RepoToEntity;
 
+import dbridge.analysis.eqsql.EqSQLDriver;
+import io.geetam.github.CMDOptions;
+import mytest.debug;
 import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.Attribute;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Signature;
 import org.w3c.dom.Attr;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import static org.apache.bcel.Repository.lookupClass;
 
 public class RepoToEntity {
     public static String getEntityForRepo(String repo) {
         try {
-            JavaClass repocls = Repository.lookupClass(repo);
+            debug d = new debug("RepoToEntity.java", "getEntityForRepo");
+            String benchDir = CMDOptions.benchDir;
+            String pwd = System.getProperty("user.dir");
+            d.dg("pwd: " + pwd);
+            d.dg("repo: " + repo);
+            String benchDirAbs = null;
+            if(benchDir.startsWith("/")) {
+                benchDirAbs = benchDir;
+            } else {
+                benchDirAbs = pwd + (pwd.endsWith("/") == false ? "/" : "") + benchDir + (benchDir.endsWith("/") ? "" : "/");
+            }
+            d.dg("benchDirAbs: " + benchDirAbs);
+            URL[] classLoaderURLs = new URL[]{new URL("file://" + benchDirAbs)};
+            URLClassLoader urlClassLoader = new URLClassLoader(classLoaderURLs);
+            Class<?> repoClass = urlClassLoader.loadClass(repo);
+            JavaClass repocls = Repository.lookupClass(repoClass);
             Attribute[] atts = repocls.getAttributes();
             for(Attribute att : atts) {
                 if(att instanceof Signature) {
@@ -26,6 +48,8 @@ public class RepoToEntity {
             }
 
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
         return null;
