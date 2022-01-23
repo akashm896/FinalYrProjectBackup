403f319e9efe31a70a7621864586ed002a3715f2
Geetam Chawla <geetam.chawla@gmail.com>
2022-01-24 00:20:59 +0530

    Add Licences

--- src/main/java/dbridge/analysis/eqsql/analysis/Utils.java
+++ src/main/java/dbridge/analysis/eqsql/analysis/Utils.java
@@ -1,3 +1,40 @@
+/*
+Copyright 2020 IIT Bombay.
+
+This software is dual-licensed under commercial and open source licenses.
+For open source use, this software is available under LGPL v3 license
+(https://www.gnu.org/licenses/lgpl-3.0.en.html). For commercial uses
+(beyond those permitted as per LGPL v3), contact dbridge@cse.iitb.ac.in.
+*/
+/*
+
+MIT License
+
+Copyright (c) 2022 Indian Institute of Science, Bangalore
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE
+SOFTWARE.
+
+*/
 package dbridge.analysis.eqsql.analysis;
 
 import com.rits.cloning.Cloner;
